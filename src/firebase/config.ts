1632a639725f00292e547892d7483d0647d76284
Diego Rivera <desarrollonaxde@gmail.com>
2026-02-26 02:09:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4920931495-1d74b",
+  "appId": "1:796856232481:web:ea19082cec9dd705f65160",
+  "apiKey": "AIzaSyB2JrU3Zm6ExlRmM-jxbbp4O7qiFthcGOo",
+  "authDomain": "studio-4920931495-1d74b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "796856232481"
+};
